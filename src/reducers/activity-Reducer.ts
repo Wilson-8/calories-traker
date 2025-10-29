581c8374a0e9b5af425c2d2d89c30d7e2b0a23b4
wilson-8 <wilrosario1212@gmail.com>
2025-10-29 16:37:24 -0400

    cap-155 form edit

--- src/reducers/activity-Reducer.ts
+++ src/reducers/activity-Reducer.ts
@@ -23,9 +23,18 @@ export const activityReducer = (
 
     if (action.type === 'Save-activity') {
         
+        let updatedActivities: Activity[] = []
+        if (state.activeId){
+           updatedActivities = state.activities.map(activity => (activity.id === state.activeId ? action.payload.newActivity : activity))
+        }else{
+           updatedActivities = [...state.activities, action.payload.newActivity]
+        }
+
      return{
          ...state,
-         activities: [...state.activities, action.payload.newActivity]
+         activities: updatedActivities,
+         activeId:''
+
      }
     }
 
