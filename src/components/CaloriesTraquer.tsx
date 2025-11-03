594ad548545a396e654059299d922f87cfb15d3f
wilson-8 <wilrosario1212@gmail.com>
2025-11-03 16:39:00 -0400

    cap-165 all done to deploy

--- src/components/CaloriesTraquer.tsx
+++ src/components/CaloriesTraquer.tsx
@@ -1,4 +1,6 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
+import CaloriesDisplay from "./CaloriesDisplay"
 
 type CaloriesTraquerProps ={
     activities: Activity[]
@@ -6,9 +8,31 @@ type CaloriesTraquerProps ={
 
 export default function CaloriesTraquer ({activities} : CaloriesTraquerProps) {
 
+    const caloriesComsumed = useMemo(() => activities.reduce((total, activities) => activities.category === 1 ? total + activities.calorias : total, 0), [activities])
+
+    const caloriesBurned = useMemo(() => activities.reduce((total, activities) => activities.category === 2 ? total + activities.calorias : total, 0), [activities])
+
+    const consumeNeto = useMemo(() => caloriesComsumed - caloriesBurned, [activities])
     return (
         <>
        <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
+       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+         <CaloriesDisplay
+        calories={caloriesComsumed}
+        text={"Consumindas"}
+        />
+
+
+        <CaloriesDisplay
+        calories={caloriesBurned}
+        text={"Ejercicio"}
+        />
+
+        <CaloriesDisplay
+        calories={consumeNeto}
+        text={"Diferencia"}
+        />
+       </div>
         </>
     )
 }
